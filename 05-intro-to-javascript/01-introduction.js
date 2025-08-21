91edf64680183e48d482bc2f7891bea7170d647f
fuadinadhif <nadhiffuadi@gmail.com>
2025-08-21 23:56:21 +0700

    add new materials

---
+++ 05-intro-to-javascript/01-introduction.js
@@ -0,0 +1,25 @@
+const username = "Bobby";
+const age = 28;
+const address = "Pogung Baru Blok F 77 Sinduadi, Mlati, Sleman, DIY";
+
+// console.log("Selamat malam, " + username);
+// console.log(typeof 42); // number
+// console.log(typeof address); // string
+
+let score = 0;
+
+// user klik tikus
+score = score + 1;
+
+// klik tikus lagi
+score = score + 1;
+
+// console.log(score);
+
+const day = "Sunday";
+
+if (day === "Friday") {
+  console.log("Horee");
+} else {
+  console.log("Huu");
+}
---
+++ 05-intro-to-javascript/NOTES.md
@@ -0,0 +1,8 @@
+# NOTES
+
+## JavaScript Keywords
+
+1. `const`: Untuk membuat variable
+2. `console.log`: Untuk menampilkan value di layar terminal
+3. `typeof`: Untuk memeriksa tipe data sebuah value
+4. `document.getElementById`: Untuk mengambil element HTML untuk dibawa ke JavaScript
