ad79a421fc6af636d310c361a202186b9667a47c
fuadinadhif <nadhiffuadi@gmail.com>
2025-09-11 20:11:19 +0700

    change nav color

--- projects/javanice/src/app/page.tsx
+++ projects/javanice/src/app/page.tsx
@@ -12,32 +12,32 @@ export default function Home() {
         </h1>
         <ul className="flex gap-[76px]">
           <li>
-            <Link href="/" className="text-h4">
+            <Link className="text-h4 text-old-brown-1" href="/">
               Home
             </Link>
           </li>
           <li>
-            <Link href="/products" className="text-h4">
+            <Link className="text-h4 text-old-brown-1" href="/products">
               Product
             </Link>
           </li>
           <li>
-            <Link className="text-h4" href="/services">
+            <Link className="text-h4 text-old-brown-1" href="/services">
               Service
             </Link>
           </li>
           <li>
-            <Link className="text-h4" href={"/about"}>
+            <Link className="text-h4 text-old-brown-1" href={"/about"}>
               About
             </Link>
           </li>
           <li>
-            <Link className="text-h4" href={"/contact"}>
+            <Link className="text-h4 text-old-brown-1" href={"/contact"}>
               Contact
             </Link>
           </li>
           <li>
-            <Link className="text-h4" href={"/blog"}>
+            <Link className="text-h4 text-old-brown-1" href={"/blog"}>
               Blog
             </Link>
           </li>
